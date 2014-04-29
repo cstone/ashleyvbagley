bd6e718389e32daadf7f463466d4de85257269d3
Cavin <cstone2003@hotmail.com>
2014-04-28 21:12:01 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ashleyvbagley::Application.config.secret_token = '99264ea7278b980b45cc79c59a3f8af6e858aff11ef0eaa952b30cd3d7fc7316e49dab09b8eafc7d41aa8ee413df2dd1b637958b4cc549cf9c507422f1548004'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ashleyvbagley::Application.config.session_store :cookie_store, key: '_ashleyvbagley_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ashleyvbagley::Application.config.session_store :active_record_store
