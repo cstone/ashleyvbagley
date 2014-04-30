d6012d52c082bbec8289492dabc7897c3fd94dc0
Cavin <cstone2003@hotmail.com>
2014-04-29 20:01:20 -0500

    Add validations and scopes to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,11 @@
 class Article < ActiveRecord::Base
-  attr_accessible :active, :allow_comments, :article_image, :body, :title
+  attr_accessible :article_image, :body, :title, :remove_article_image, :allow_comments
+
+  mount_uploader :article_image, ArticleImageUploader
+
+  #validations
+  validates_presence_of :title, :body
+  validates_uniqueness_of :title
+
+  default_scope order('created_at DESC')
 end
