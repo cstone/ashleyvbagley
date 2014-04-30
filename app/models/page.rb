1e4c8e71e7a4a9f487e170ec561351b2d88c43e5
Cavin <cstone2003@hotmail.com>
2014-04-29 20:01:50 -0500

    Add validations and scopes to page model

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,12 @@
 class Page < ActiveRecord::Base
-  attr_accessible :content, :page_image, :permalink, :title
+  attr_accessible :content, :name, :permalink, :page_image, :remove_page_image
+
+  mount_uploader :page_image, PageImageUploader
+
+  validates_uniqueness_of :permalink
+  validates_presence_of :name, :permalink, :content
+
+  def to_param
+    permalink
+  end
 end
