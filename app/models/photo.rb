4b916b52952111bd813a9ab394aae26e48c86d28
Cavin <cstone2003@hotmail.com>
2014-04-29 20:01:05 -0500

    Add validations and scopes to photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,9 @@
 class Photo < ActiveRecord::Base
-  attr_accessible :allow_comments, :description, :photo_image, :title
+  attr_accessible :description, :photo_image, :title, :remove_photo_image, :allow_comments
+
+  mount_uploader :photo_image, PhotosImageUploader
+
+
+  #validations
+  validates_presence_of :title, :description, :photo_image
 end
