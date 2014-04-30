888604887e416884c81bd58f595073809f24bcbe
Cavin <cstone2003@hotmail.com>
2014-04-30 11:15:00 -0500

    Update Video resource in ActiveAdmin

--- app/admin/videos.rb
+++ app/admin/videos.rb
@@ -1,3 +1,36 @@
 ActiveAdmin.register Video do
-  
+
+  index do
+    selectable_column
+    column :title
+    column :source
+    column :allow_comments
+    default_actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :description
+      f.input :source
+      f.input :allow_comments
+    end
+    f.actions
+  end
+
+  show do |video|
+    attributes_table do
+      row :id
+      row :title
+      row :description do
+        simple_format video.description
+      end
+      row :source
+      row :allow_comments
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+
 end
