19922f4f3580225064c33978d2af8dd4e7e2cbc7
Cavin <cstone2003@hotmail.com>
2014-04-30 00:04:28 -0500

    Add ActiveAdmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Ashleyvbagley::Application.routes.draw do
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   root :to => 'home#index'
 
   get '/gallery(/:page)', to: 'photos#gallery', as: :gallery
