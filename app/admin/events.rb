41598bf617ef991e4a70ad826f11fb5c60505c18
Cavin <cstone2003@hotmail.com>
2014-04-30 11:14:42 -0500

    Update Event resource in ActiveAdmin

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -1,3 +1,50 @@
 ActiveAdmin.register Event do
-  
+
+  index do
+    selectable_column
+    column :title
+    column :time do |model|
+      model.time.to_formatted_s(:my_format)
+    end
+    default_actions
+  end
+
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :description
+      f.input :venue
+      f.input :address
+      f.input :city
+      f.input :state
+      f.input :zip
+      f.input :url
+      f.input :time
+    end
+    f.actions
+  end
+
+
+  show do |event|
+    attributes_table do
+      row :id
+      row :title
+      row :description do
+        event.description.html_safe
+      end
+      row :venue
+      row :address
+      row :city
+      row :state
+      row :zip
+      row :url
+      row :time do
+        event.time.to_formatted_s(:date_time)
+      end
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
 end
