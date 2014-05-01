2b4475d7d6f2d8fb60cdd2860bc1a14a4a33bfd1
Cavin <cstone2003@hotmail.com>
2014-04-30 19:10:33 -0500

    Update pages views in ActiveAdmin

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -5,23 +5,31 @@ ActiveAdmin.register Page do
 
   index do
     selectable_column
-    column :title
+    column :page_image do |page|
+      if page.page_image.url
+        image_tag page.page_image.url(:thumb)
+      end
+    end
+    column :title do |content|
+      content.title.truncate(50).html_safe
+    end
     column :permalink
+    column :content do |content|
+      content.content.truncate(75).html_safe
+    end
     default_actions
   end
 
-
   form do |f|
-    f.inputs "Pages", :multipart => true do
+    f.inputs do
       f.input :title
       f.input :permalink
-      f.input :content
+      f.input :content, as: :html_editor
       f.input :page_image, :as => :file, :hint => f.object.page_image.present? \
     ? f.template.image_tag(f.object.page_image.url(:thumb))
       : f.template.content_tag(:span, "no photo yet")
       f.input :page_image_cache, :as => :hidden
       f.input :remove_page_image, as: :boolean, label: 'Check here to remove the current image.'
-
     end
     f.actions
   end
@@ -35,9 +43,9 @@ ActiveAdmin.register Page do
       row :content do
         simple_format page.content
       end
-      if page.page_image
-        row :page_image do
-          image_tag(page.page_image.url)
+      row :page_image do
+        if page.page_image.url
+          image_tag page.page_image.url
         end
       end
       row :created_at
