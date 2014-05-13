f567e508f661999d9a04b483c45b418ba5c588ec
Cavin <cstone2003@hotmail.com>
2014-05-13 17:32:57 -0500

    Change home route pages#show view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Ashleyvbagley::Application.routes.draw do
 
   mount Ckeditor::Engine => '/ckeditor'
 
-  root :to => 'home#index'
+  root :to => 'pages#show', id: 'home'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
