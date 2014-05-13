a7dd59bd870b6e43a638d12031b66c6402c25938
Cavin <cstone2003@hotmail.com>
2014-05-13 15:19:33 -0500

    Add get_content and get_title methods

--- app/models/page.rb
+++ app/models/page.rb
@@ -6,6 +6,18 @@ class Page < ActiveRecord::Base
   validates_uniqueness_of :permalink
   validates_presence_of :title, :permalink, :content
 
+  def self.get_content permalink
+    page = Page.where(permalink: permalink).first
+    return page.content if !page.nil?
+    return "Content missing for key #{permalink}"
+  end
+
+  def self.get_title permalink
+    page = Page.where(permalink: permalink).first
+    return page.title if !page.nil?
+    return "Title missing for key #{permalink}"
+  end
+
   def to_param
     permalink
   end
