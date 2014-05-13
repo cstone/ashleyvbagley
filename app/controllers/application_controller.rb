a8404c1ce12d67d07d7344d2f454dce3c37b86e8
Cavin <cstone2003@hotmail.com>
2014-05-13 15:31:26 -0500

    Comment out youtube_url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     @facebook_url = DynamicContent.get_value(:facebook_url)
     @twitter_url = DynamicContent.get_value(:twitter_url)
     @instagram_url = DynamicContent.get_value(:instagram_url)
-    @youtube_url = DynamicContent.get_value(:youtube_url)
+    # @youtube_url = DynamicContent.get_value(:youtube_url)
     @site_title = DynamicContent.get_value(:site_title)
     @home_title = Page.get_title(:home)
     @home_content = Page.get_content(:home).html_safe
